1a3d2f620aa43e1f5bf1c0c8f531c6ee36dcd191
Btarantino94 <Btarantino94@gmail.com>
2017-10-13 01:44:18 -0400

    fixing db

--- lib/inc/db.inc.php
+++ lib/inc/db.inc.php
@@ -4,10 +4,10 @@ try{
 	// MAMP
   // $db = new PDO('mysql:host=localhost;dbname=btarantino_Challenge_one', 'root', 'root');
   // Server
-  $db = new PDO('mysql:host=localhost;dbname=btarantino_Challenge_one', 'r2hstudent', 'SbFaGzNgGIE8kfP');
+  $db = new PDO('mysql:host=localhost;dbname=btarantino', 'r2hstudent', 'SbFaGzNgGIE8kfP');
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  //$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 } catch (PDOException $e) {
   
